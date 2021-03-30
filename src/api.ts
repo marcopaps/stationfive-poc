ac68c20c27d5d32e7b46821e565d2d82b90aca28
Marco Budiongan <marcobdngn@gmail.com>
2021-03-30 13:26:49 +0800

    Added more sample menu and rules

--- src/api.ts
+++ src/api.ts
@@ -22,6 +22,29 @@ export default {
       { id: '303', value: 'Vegetable spring rolls' },
       { id: '304', value: 'Steamed rice' },
     ],
+
+    /* Added 4th and 5th group to showcase scalability
+     * Real world context is not considered for sample data
+     * (e.g Vegetarian is not compatible with unli pork and so on)
+     *
+     * This can be removed or changed. Refresh page after.
+     */
+    // 4th group of radio-buttons
+    [
+      { id: '401', value: 'Pinakbet' },
+      { id: '402', value: 'Pork adobo' },
+      { id: '403', value: 'Chicken silog' },
+      { id: '404', value: 'Pork sinigang' },
+      { id: '405', value: 'Pancit canton' },
+      { id: '406', value: 'Unli pork' },
+    ],
+
+    // 5th group of radio-buttons
+    [
+      { id: '501', value: 'Romantic baboy' },
+      { id: '502', value: 'Unli wings' },
+      { id: '503', value: 'Fat Dois' },
+    ],
   ],
   rules: {
     // 'Vegetarian' is NOT compatible with 'Cashew chicken', 'Sweet and sour pork', 'Massaman beef', 'Oyster sauce'
@@ -29,11 +52,14 @@ export default {
     // 'Nut allergy' is NOT compatible with 'Cashew chicken', 'Peanut sauce',
     102: [201, 301],
     // 'Halal' is NOT compatible with 'Sweet and sour pork',
-    103: [202],
+    103: [202, 402, 404, 501, 406],
     // 'Vegetable fried rice' is NOT compatible with 'Steamed rice' (you don't need more rice... carb overload),
     204: [304],
     // 'Pad thai' is NOT compatible with 'Steamed rice' (Pad thai comes with noodles),
     205: [304],
+    // Added sample rules
+    303: [406, 502],
+    403: [502],
   },
 };
 
